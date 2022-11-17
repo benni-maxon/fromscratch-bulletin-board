8800af367acdf7a454f08c26bcd37e9f0ccecf49
benni maxon <benni.maxon@gmail.com>
2022-11-16 16:54:48 -0800

    tested built and tested getPosts()

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 /* Imports */
 
+import { getPosts } from './fetch-utils.js';
+
 /* Get DOM Elements */
 
 /* State */
 
 /* Events */
-
+window.addEventListener('load', () => {
+    // console.log(getPosts());
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,8 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxdmhieXBxbWZsdnp3cHV4b2hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMzAsImV4cCI6MTk4MzY4NDAzMH0.69mAZ8sZWHEPxgHYg8wvmHJc4GleoS6fqfGdJQOWGno';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getPosts() {
+    const response = await client.from('posts').select('*');
+    return response.data;
+}
