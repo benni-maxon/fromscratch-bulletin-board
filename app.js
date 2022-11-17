f17f14441fb7c2ffab2e37290de05e89c676894e
Akilah Powell <atpjun@gmail.com>
2022-11-16 21:12:27 -0400

    added DOM elements, created async load data function to loop and render

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 /* Imports */
 
 import { getPosts } from './fetch-utils.js';
+import { renderPostIt } from './render-utils.js';
 
 /* Get DOM Elements */
-
+const bulletin = document.getElementById('bulletin-board');
+const authButton = document.getElementById('auth-button');
+const createButton = document.getElementById('create');
 /* State */
 
 /* Events */
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
     // console.log(getPosts());
+    const posts = await getPosts();
+    for (let post of posts) {
+        const postDiv = renderPostIt(post);
+        bulletin.append(postDiv);
+    }
 });
+
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
