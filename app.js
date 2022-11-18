fb74648b83d91e3997fa068c8959daaab3098828
benni maxon <benni.maxon@gmail.com>
2022-11-17 17:32:54 -0800

    finished app.js functionality MVP with zero styling

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 /* Imports */
-
 import { getPosts, getUser, logout } from './fetch-utils.js';
 import { renderPostIt } from './render-utils.js';
 
 /* Get DOM Elements */
 const bulletin = document.getElementById('bulletin-board');
 const authButton = document.getElementById('auth-button');
-const createButton = document.getElementById('create');
-/* State */
+const createButton = document.getElementById('create-button');
 
 /* Events */
 window.addEventListener('load', async () => {
@@ -23,9 +21,9 @@ window.addEventListener('load', async () => {
         authButton.textContent = 'Login';
     }
 
-    // createButton.addEventListener('click', () => {
-    //     location.replace('/create');
-    // });
+    createButton.addEventListener('click', () => {
+        location.replace('/create');
+    });
 
     const posts = await getPosts();
     for (let post of posts) {
@@ -33,7 +31,3 @@ window.addEventListener('load', async () => {
         bulletin.append(postDiv);
     }
 });
-
-/* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
